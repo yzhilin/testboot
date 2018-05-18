d108905ba27e20b1ea42e62325ee1c5f97e395fe
yangzhilin <1074083778@qq.com>
2018-05-18 17:57:58 +0800

    测试代码

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,26 +1,19 @@
 package hello;
 
-
-import filter.Myfilter;
-import org.springframework.beans.BeansException;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+//        SpringApplication springApplication = new SpringApplication(MySpringConfiguration.class);
+//        springApplication.setWebApplicationType(WebApplicationType.SERVLET);
+//        springApplication.run(args);
+        SpringApplication.run(Application.class,args);
 
-    @Bean
-    public Myfilter getBean(){
-        return  new Myfilter();
     }
 
 }
